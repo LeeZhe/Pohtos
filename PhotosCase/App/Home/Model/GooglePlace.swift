749e0b64f36cef53c86b624c87f360aafbb7b1ae
KiddieBao <zhumingbao@cangoonline.com>
2018-04-11 20:38:48 +0800

    no message

---
+++ PhotosCase/App/Home/Model/GooglePlace.swift
@@ -0,0 +1,13 @@
+//
+//  GooglePlace.swift
+//  PhotosCase
+//
+//  Created by KiddieBao on 2018/4/11.
+//  Copyright © 2018年 Kiddie. All rights reserved.
+//
+
+import UIKit
+
+class GooglePlace: NSObject {
+
+}
